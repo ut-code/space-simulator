3f5cc982af1a652a86cc9da4dabb00f68c46cd76
shirokuma222 <koooshirokuma@gmail.com>
2026-02-20 00:11:17 +0900

    修正しました

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Simulation from "@/pages/Simulation";
+import PlanetsArrangement from "@/pages/PlanetsArrangement";
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/simulation" element={<Simulation />} />
+      <Route path="/planets-arrangement" element={<PlanetsArrangement />} />
     </Routes>
   </BrowserRouter>
 );
