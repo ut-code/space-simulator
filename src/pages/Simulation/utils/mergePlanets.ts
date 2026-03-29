eb20f4b724e4e6fd41bde323de214acf4db92a4d
tknkaa <tknkaa@users.noreply.github.com>
2026-03-29 12:06:20 +0800

    perf: convert jpeg to avif

--- src/pages/Simulation/utils/mergePlanets.ts
+++ src/pages/Simulation/utils/mergePlanets.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import moonTexture from "@/assets/960px-Moon_texture.jpg";
+import moonTexture from "@/assets/960px-Moon_texture.avif";
 import type { Planet } from "@/types/planet";
 
 export function mergePlanets(
