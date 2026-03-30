e80c20fb47821acbed66025afe7ae1d33d227464
tknkaa <tknkaa@users.noreply.github.com>
2026-03-30 23:11:39 +0800

    feat: add 404 page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,60 @@
+import { Stars } from "@react-three/drei";
+import { useNavigate } from "react-router-dom";
+import ThreeCanvas from "@/pages/Home/Canvas";
+import HomeScene from "@/pages/Home/Scene";
+
+export default function Page() {
+	const navigate = useNavigate();
+
+	return (
+		<div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+			<ThreeCanvas>
+				<Stars
+					radius={100} // 星が広がる球の半径
+					depth={50} // 奥行き
+					count={5000} // 星の数
+					factor={4} // 星のサイズ
+					saturation={0} // 色の鮮やかさ
+					fade // 遠くの星をフェード
+					speed={1} // 動き速度
+				/>
+				<HomeScene />
+			</ThreeCanvas>
+
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white">
+				<h1
+					className="
+						text-7xl
+						font-extrabold
+						tracking-widest
+						text-white
+						mb-10
+						animate-pulse
+						drop-shadow-[0_0_25px_rgba(255,255,255,0.8)]
+					"
+				>
+					NOT FOUND
+				</h1>
+				<button
+					type="button"
+					onClick={() => navigate("/")}
+					className="
+						px-10 py-4
+						text-xl font-semibold
+						text-white
+						bg-blue-500
+						rounded-xl
+						shadow-lg shadow-blue-500/40
+						border border-blue-300
+						transition
+						hover:bg-blue-400
+						hover:scale-105
+						active:scale-95
+					"
+				>
+					Return to Start Page
+				</button>
+			</div>
+		</div>
+	);
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
+import NotFound from "@/pages/NotFound";
 import Simulation from "@/pages/Simulation";
 
 export const AppRouter = () => (
@@ -7,6 +8,7 @@ export const AppRouter = () => (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/simulation" element={<Simulation />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	</BrowserRouter>
 );
