44bc8e03b1e158fd9ab98dbb6bc176c437e93209
faithia-anastasia <211831874+faithia-anastasia@users.noreply.github.com>
2026-04-04 15:57:12 +0900

    feat: add planet textures

--- src/data/solarSystem.ts
+++ src/data/solarSystem.ts
@@ -1,7 +1,11 @@
 import * as THREE from "three";
 import jupiterTexture from "@/assets/2k_jupiter.avif";
 import marsTexture from "@/assets/2k_mars.avif";
+import mercuryTexture from "@/assets/2k_mercury.avif";
+import neptuneTexture from "@/assets/2k_neptune.avif";
+import saturnTexture from "@/assets/2k_saturn.avif";
 import sunTexture from "@/assets/2k_sun.avif";
+import uranusTexture from "@/assets/2k_uranus.avif";
 import venusTexture from "@/assets/2k_venus_atmosphere.avif";
 import earthTexture from "@/assets/earth_atmos_2048.avif";
 import { G } from "@/pages/Simulation/core/GravitySystem";
@@ -33,7 +37,7 @@ const sun: Planet = {
 const mercury: Planet = {
 	id: "mercury",
 	name: "Mercury",
-	texturePath: earthTexture,
+	texturePath: mercuryTexture,
 	rotationSpeedY: 2,
 	radius: 0.76, // 0.38 × 2
 	width: 64,
@@ -98,7 +102,7 @@ const jupiter: Planet = {
 const saturn: Planet = {
 	id: "saturn",
 	name: "Saturn",
-	texturePath: earthTexture,
+	texturePath: saturnTexture,
 	rotationSpeedY: 2,
 	radius: 18.9, // 9.45 × 2
 	width: 64,
@@ -111,7 +115,7 @@ const saturn: Planet = {
 const uranus: Planet = {
 	id: "uranus",
 	name: "Uranus",
-	texturePath: earthTexture,
+	texturePath: uranusTexture,
 	rotationSpeedY: 2,
 	radius: 8.0, // 4.0 × 2
 	width: 64,
@@ -124,7 +128,7 @@ const uranus: Planet = {
 const neptune: Planet = {
 	id: "neptune",
 	name: "Neptune",
-	texturePath: earthTexture,
+	texturePath: neptuneTexture,
 	rotationSpeedY: 2,
 	radius: 7.8, // 3.9 × 2
 	width: 64,
