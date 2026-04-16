394d22ec571420698c77f7799c22bc19ce73d0a9
Endo-Yuanteng <210224788+Endo-Yuanteng@users.noreply.github.com>
2026-04-16 18:59:31 +0900

    fix: correct radix switch import

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,4 +1,4 @@
-import { Switch as SwitchPrimitive } from "radix-ui";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
 import type * as React from "react";
 
 import { cn } from "@/lib/utils";
